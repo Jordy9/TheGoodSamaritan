9c1461954564e47bc2b2149c88d98f9b3b5d76cf
Jordy9 <xilero23@gmail.com>
2021-12-14 23:59:39 -0400

    Vamos a ver

--- src/Components/dashboard/Dashboard.jsx
+++ src/Components/dashboard/Dashboard.jsx
@@ -9,7 +9,7 @@ import { ModalMiniSerie } from './modal/ModalMiniSerie'
 export const Dashboard = () => {
     return (
         <>
-            <h1 style = {{marginTop: '70px'}}>Inicios</h1>
+            <h1 style = {{marginTop: '70px'}}>Inicioass</h1>
             <Carousel />
             <CarrouselEvents />
             <CarouselSketch />
