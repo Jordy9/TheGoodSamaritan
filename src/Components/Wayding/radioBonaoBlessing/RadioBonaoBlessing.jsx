5fc5174bea9a5bb3cea6061a404c29c2da63bb56
Jordy9 <xilero23@gmail.com>
2022-02-06 21:55:33 -0400

    Veamos si funciona ahora

--- src/Components/Wayding/radioBonaoBlessing/RadioBonaoBlessing.jsx
+++ src/Components/Wayding/radioBonaoBlessing/RadioBonaoBlessing.jsx
@@ -17,7 +17,7 @@ export const RadioBonaoBlessing = () => {
 
             <div className="row">
                 <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 d-flex justify-content-center align-items-center">
-                    <audio crossorigin controls autoPlay src={RadioBonaoBendicion}></audio>
+                    <audio crossorigin = {false} controls autoPlay src={RadioBonaoBendicion}></audio>
                 </div>
             </div>
 
