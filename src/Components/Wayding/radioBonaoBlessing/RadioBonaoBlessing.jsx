5f43d133cfe2059513e8f6aaab30c2941ffe0416
Jordy9 <xilero23@gmail.com>
2022-02-06 20:23:12 -0400

    Intento de funcionalidad de los links

--- src/Components/Wayding/radioBonaoBlessing/RadioBonaoBlessing.jsx
+++ src/Components/Wayding/radioBonaoBlessing/RadioBonaoBlessing.jsx
@@ -3,7 +3,7 @@ import RadioBonaoBendi from '../../../heroes/BonaoBendicionn.jpg'
 
 export const RadioBonaoBlessing = () => {
     
-    const RadioBonaoBendicion = 'http://178.33.166.7:7504/;stream.nsv'
+    const RadioBonaoBendicion = 'http://51.15.160.78:8171/live'
 
     return (
         <div className='container'>
