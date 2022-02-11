5b897626ffe8d20293a3c1a37f20fab81959dbc0
Jordy9 <xilero23@gmail.com>
2022-02-11 19:57:15 -0400

    Veamos a ver si funciona

--- src/Components/Wayding/messages/Messages.jsx
+++ src/Components/Wayding/messages/Messages.jsx
@@ -14,8 +14,7 @@ export const Messages = () => {
                         <div className = 'my-5'>
                             <div className="row my-5">
                                 <div className="shadow bg-dark p-4 col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 d-flex justify-content-center align-items-center">
-                                    {/* <ReactPlayer width = '100vw' height = '100vh' url={Telebendicion} controls playing loop /> */}
-                                    <iframe title='lol' allow-same-origin src={Telebendicion} />;
+                                    <ReactPlayer width = '100vw' height = '100vh' url={Telebendicion} controls playing loop />
                                 </div>
                             </div>
                         </div>
