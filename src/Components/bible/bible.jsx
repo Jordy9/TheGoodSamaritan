2eb187e4f4e3e6a4d7e08457d12522a679e6bc74
Jordy9 <xilero23@gmail.com>
2022-02-17 21:30:51 -0400

    .

--- src/Components/bible/bible.jsx
+++ src/Components/bible/bible.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { Nav, Navbar } from 'react-bootstrap'
 import { useSelector } from 'react-redux'
 import { useDispatch } from 'react-redux'
+import { startGetNotes } from '../../action/notas'
 import { BookClear, getBook } from '../../action/verseofTheDay'
 import { Antiguotestamento } from '../../Antiguotestamento'
 import { scrollToTopAnimated } from '../../helper/ScrollToBottom'
@@ -81,6 +82,11 @@ export const Bible = () => {
         dispatch(BookClear())
       }
     }, [dispatch])
+
+    useEffect(() => {
+        dispatch(startGetNotes())
+    }, [dispatch])
+    
     
     
   return (
