51b1fcbdf3ccc0501def93eec82d5d1ea105e1b1
Jordy9 <xilero23@gmail.com>
2022-03-27 23:28:47 -0400

    .

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -55,7 +55,7 @@ export const AppRouter = () => {
     const dispatch = useDispatch()
     const {checking, uid} = useSelector(state => state.auth)
 
-    const {socket, online, conectarSocket, desconectarSocket} = useSocket('http://localhost:4000')
+    const {socket, online, conectarSocket, desconectarSocket} = useSocket('https://good-samaritan-backend.herokuapp.com')
 
     const token = localStorage.getItem('tokenn')
 
