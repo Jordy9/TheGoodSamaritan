91be5e3c66de869a0cd23b6c44ad0c5d2551fd46
Jordy9 <xilero23@gmail.com>
2022-05-07 15:04:05 -0400

    .

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,9 @@ import './index.css';
 import { GoodSamaritan } from './GoodSamaritan';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import { UseRedirectToHttps } from './hooks/UseRedirectToHttps';
+
+UseRedirectToHttps()
 
 ReactDOM.render(
   <React.StrictMode>
