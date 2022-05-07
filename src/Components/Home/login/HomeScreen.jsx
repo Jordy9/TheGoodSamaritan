762075579778e30b35642b5c436a2ad210598b09
Jordy9 <xilero23@gmail.com>
2022-05-07 14:50:21 -0400

    .

--- src/Components/Home/login/HomeScreen.jsx
+++ src/Components/Home/login/HomeScreen.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import { UseRedirectToHttps } from '../../../hooks/UseRedirectToHttps'
 import { Home } from './home/Home'
 
 export const HomeScreen = () => {
 
-    UseRedirectToHttps()
+
     return (
         <>
             <Home />
