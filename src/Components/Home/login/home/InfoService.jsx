97c9b384e4bd6130084ef54394bb2ce1a44b73a8
Jordy9 <xilero23@gmail.com>
2022-05-08 21:33:08 -0400

    Mejoras en los modales de deseas seguimiento y en la opcion de discipulado, ya tiene su funcion aplicada.

--- src/Components/Home/login/home/InfoService.jsx
+++ src/Components/Home/login/home/InfoService.jsx
@@ -28,12 +28,12 @@ export const InfoService = () => {
 
                     <div className="col-xs-2 col-sm-6 col-md-6 col-lg-3 col-xl-3 p-4 hom" onClick={redirectToAmanecer}>
                         <i className="fa-solid fa-church d-flex justify-content-center" style = {{fontSize: '32px'}}></i>
-                        <h4 className = 'text-center'>Ministerio El Amanecer de la Esperanza, NY</h4>
+                        <h4 className = 'text-center'>Ministerios El Amanecer de la Esperanza, NY</h4>
                     </div>
 
                     <div className="col-xs-2 col-sm-6 col-md-6 col-lg-3 col-xl-3 p-4 hom" onClick={redirectToAPR}>
                         <i className="fa-solid fa-place-of-worship d-flex justify-content-center" style = {{fontSize: '32px'}}></i>
-                        <h4 className = 'text-center'>Ministerio A Los Pies del Rey, Murcia España</h4>
+                        <h4 className = 'text-center'>Ministerios A Los Pies del Rey, Murcia España</h4>
                     </div>
                 </div>
             </div>
