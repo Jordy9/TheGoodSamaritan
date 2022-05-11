4dd850aa1dbccee17aba7b705ae21f7d7acbe46b
Jordy9 <xilero23@gmail.com>
2022-05-11 09:34:31 -0400

    .

--- src/Components/Home/login/navbar/NavbHomeScreen.jsx
+++ src/Components/Home/login/navbar/NavbHomeScreen.jsx
@@ -56,9 +56,8 @@ export const NavbHomeScreen = () => {
   }, [width]);
   
     return (
-        <div hidden = {state}>
-            <Navbar className = 'col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12' expand="lg" bg = 'dark' variant="dark">
-                <Container fluid = {(width <= 991) ? true : false}>
+            <Navbar id='over' hidden = {state} className = 'col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12' bg = 'dark' variant="dark">
+                <Container>
                     <Navbar.Brand style = {{cursor: 'pointer', margin: 0, padding: 0}} >
                         <span className = 'Navb-tittle d-flex justify-content-end align-items-center'>
                             <img src={logo} className='img-fluid' style={{width: 'auto', height: '40px', marginLeft: (width > 991) ?'60px' : '20px'}} alt="" />
@@ -98,6 +97,5 @@ export const NavbHomeScreen = () => {
                             }
                 </Container>
             </Navbar>
-        </div>
     )
 }
--- src/Components/Home/login/sidebar/Sidebar.jsx
+++ src/Components/Home/login/sidebar/Sidebar.jsx
@@ -35,9 +35,9 @@ export const Sidebar = () => {
 
     return (
         <>
-            <i id='logo-list' style = {{cursor: 'pointer', color: 'white', fontSize: '25px', marginLeft: '30px'}} onClick={handleShow} className="bi bi-list"></i>
+            <i id='logo-list' style = {{cursor: 'pointer', color: 'white', fontSize: '25px'}} onClick={handleShow} className="bi bi-list"></i>
 
-            <Offcanvas scroll placement='end' show={show} onHide={handleClose}>
+            <Offcanvas placement='end' show={show} onHide={handleClose}>
                 <Offcanvas.Header closeButton>
                 <Offcanvas.Title>Dios te bendiga</Offcanvas.Title>
                 </Offcanvas.Header>
