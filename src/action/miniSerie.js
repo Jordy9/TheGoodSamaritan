3beb53af1c185cc31ab89d86ad3df328f5990e27
Jordy9 <xilero23@gmail.com>
2022-11-24 11:48:21 -0400

    Mejora en el api de pixabay

--- src/action/miniSerie.js
+++ src/action/miniSerie.js
@@ -7,7 +7,9 @@ import { Types } from "../types/Types"
 export const getImageApi = (searchParam, setnewImage, page) => {
   return async(dispatch) => {
 
-    const { data } = await axios.get(`https://pixabay.com/api/?key=31553456-6c1c8a5d0a38b1f411c5a2bbf&q=${searchParam}&page=${page || 1}&per_page=20&safesearch=true`)
+    let newSearchParams = encodeURIComponent(searchParam)
+
+    const { data } = await axios.get(`https://pixabay.com/api/?key=31553456-6c1c8a5d0a38b1f411c5a2bbf&q=${newSearchParams}&page=${page || 1}&per_page=20&safesearch=true&lang=es`)
 
     setnewImage(data)
   }
